f1c21637341da6660fb9a763bf098a4812e55e98
Bhala1987 <bhala.tr@gmail.com>
2017-11-01 10:32:13 +0000

    Update Service.java

--- src/test/java/identity/Service.java
+++ src/test/java/identity/Service.java
@@ -7,9 +7,7 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileFilter;
